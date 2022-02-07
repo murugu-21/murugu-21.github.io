259b60c3a89704c723c46a49a7364fbc6f1106da
murugu-21 <murugu2001@gmail.com>
2022-02-07 22:48:58 +0530

    gtag fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -52,12 +52,13 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    // {
-    //   resolve: `gatsby-plugin-google-analytics`,
-    //   options: {
-    //     trackingId: `ADD YOUR TRACKING ID HERE`,
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-gtag`,
+      options: {
+        trackingId: `G-EGG005JECM`,
+        head: true,
+      },
+    },
     {
       resolve: `gatsby-plugin-feed`,
       options: {
--- src/pages/index.js
+++ src/pages/index.js
@@ -11,17 +11,11 @@ const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata?.title || `Title`
   
   return (
-    <>
-      <meta
-        name="google-site-verification"
-        content="L2txHVrtaGzJOqyh2MkZ4IKEVw9V1181aZ3fflwpTjk"
-      />
-      <Layout location={location} title={siteTitle}>
-        <Seo title="All posts" />
-        <AllPosts data={data} />
-        <Bio />
-      </Layout>
-    </>
+    <Layout location={location} title={siteTitle}>
+      <Seo title="All posts" />
+      <AllPosts data={data}/>
+      <Bio />
+    </Layout>
   )
 }
 
